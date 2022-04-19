bf0d33c4971b7c20781920dac258e10316ba3aa9
João Hudson <joao.oliveira@projecti.com.br>
2022-04-19 13:08:56 -0300

    adicionando versionamento dinamico pelo github

--- client/main.js
+++ client/main.js
@@ -6,8 +6,38 @@
 
     const menuDiv = document.getElementById('menu');
     const loadingDiv = document.getElementById('loading');
+    const clientsDiv = document.getElementById('clientsDiv');
 
     const baseUrl = window.location.href;
+    const githubBaseurl = 'https://api.github.com/repos/joaohudson/';
+    
+    const infoClients = [
+        {
+            name: 'Android Version',
+            downloadName: 'YouTube Downloader and Converter.apk',
+            repository: 'youtube-downloader-converter-mobile'
+        },
+        {
+            name: 'Windows Version',
+            downloadName: 'YouTube Downloader And Converter.zip',
+            repository: 'youtube-downloader-converter-desktop'
+        }
+    ];
+
+    async function fetchClients(){
+        const clients = [];
+        for(const info of infoClients){
+            const headers = {Accept: 'application/vnd.github.v3+json'};
+            const response = await fetch(githubBaseurl + info.repository + '/releases', {headers});
+
+            if(!response.ok)
+                continue;
+
+            const releases = await response.json();
+            clients.push({name: info.name, downloadName: info.downloadName, url: releases[0].assets[0].browser_download_url});
+        }
+        return clients;
+    }
 
     function displayLoading(active){
         if(active){
@@ -85,4 +115,31 @@
             displayLoading(false);
         }
     }
+
+    async function main(){
+        const clients = await fetchClients();
+
+        for(let i = 0; i < clients.length - 1; i++){
+            const client = clients[i];
+            const a = document.createElement('a');
+            a.href = client.url;
+            a.download = client.downloadName;
+            a.innerText = client.name;
+            clientsDiv.appendChild(a);
+
+            const separator = document.createElement('label');
+            separator.id = 'separator';
+            separator.innerText = '|';
+            clientsDiv.appendChild(separator);
+        }
+
+        const client = clients[clients.length - 1];
+        const a = document.createElement('a');
+        a.href = client.url;
+        a.download = client.downloadName;
+        a.innerText = client.name;
+        clientsDiv.appendChild(a);
+    }
+
+    main();
 })();
